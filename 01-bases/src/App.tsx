26eed58a2ae056414f829bccca8dbf585801cfc8
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-01-18 13:10:55 -0600

    Initial commit

---
+++ 01-bases/src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from "./bases/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>React</h1>
+      <hr />
+      <Counter initialValue={15} />
+    </>
+  );
+}
+
+export default App;
