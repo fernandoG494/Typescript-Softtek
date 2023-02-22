e56f9b58b480c2e66b4529796d000191bdba782f
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-02-21 21:58:58 -0600

    bases section last commit

--- 01-bases/src/bases/CounterEffect.tsx
+++ 01-bases/src/bases/CounterEffect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 
 interface Props {
@@ -10,21 +10,32 @@ const MAXIMUM_VALUE = 10;
 const CounterEffect = ({ initialValue = 5 }: Props) => {
   const [counter, setCounter] = useState(initialValue);
 
+  const counterElement = useRef<HTMLHeadingElement>(null);
+
   const handleClick = () => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUM_VALUE));
   };
 
   useEffect(() => {
     if (counter < 10) return;
-    gsap.to("h2", { y: -10, duration: 0.2, ease: "ease.out" }).then(() => {
-      gsap.to("h2", { y: 0, duration: 1, ease: "bounce.out" });
-    });
+    console.log(
+      "%cSe llegó al valor máximo",
+      "color: red; background-color: black;"
+    );
+
+    const tl = gsap.timeline();
+
+    tl.to(counterElement.current, {
+      y: -10,
+      duration: 0.2,
+      ease: "ease.out",
+    }).to(counterElement.current, { y: 0, duration: 1, ease: "bounce.out" });
   }, [counter]);
 
   return (
     <>
       <h1>Counter Effect:</h1>
-      <h2>{counter}</h2>
+      <h2 ref={counterElement}>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
