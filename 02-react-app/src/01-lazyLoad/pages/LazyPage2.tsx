a0cf9a5f3e5fab10e0366a19f436a3bb0ad036aa
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-02-28 20:08:22 -0600

    Componentes lazy agregados v6

---
+++ 02-react-app/src/01-lazyLoad/pages/LazyPage2.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LazyPage2 = () => {
+  return <h1>Lazy Page 2</h1>;
+};
---
+++ 02-react-app/src/01-lazyLoad/pages/LazyPage3.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LazyPage3 = () => {
+  return <h1>Lazy Page 3</h1>;
+};
