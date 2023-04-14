be1b6375c774fc3d223f57a95b549e633b56595e
fernandoG494 <lfgc851@gmail.com>
2023-04-14 12:39:36 -0600

    update

---
+++ react-app/src/routes/routes.ts
@@ -0,0 +1,26 @@
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
+interface Route {
+    path: string,
+    Component: () => JSX.Element,
+    name: string,
+    children?: Route[]
+}
+
+export const routes: Route[] = [
+    {
+        path: '/lazy1',
+        Component: LazyPage1,
+        name: 'LazyPage-1'
+    },
+    {
+        path: '/lazy2',
+        Component: LazyPage2,
+        name: 'LazyPage-2'
+    },
+    {
+        path: '/lazy3',
+        Component: LazyPage3,
+        name: 'LazyPage-3'
+    }
+];
