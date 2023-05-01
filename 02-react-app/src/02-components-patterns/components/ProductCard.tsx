4177381db34f92752efcec4af41df9ab4e0d2fd9
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-04-30 22:47:08 -0600

    compound block finished

--- 02-react-app/src/02-components-patterns/components/ProductCard.tsx
+++ 02-react-app/src/02-components-patterns/components/ProductCard.tsx
@@ -1,32 +1,29 @@
-import { createContext } from 'react';
-import { useProduct } from '../hooks/useProduct';
-import { ProductContextProps, ProductCardProps } from '../interfaces/interfaces';
-import { ProductTitle } from './ProductTitle';
-import { ProductImage } from './ProductImage';
-import { ProductButtons } from './ProductButtons';
+import { createContext } from "react";
+import { useProduct } from "../hooks/useProduct";
+import {
+	ProductContextProps,
+	ProductCardProps,
+} from "../interfaces/interfaces";
 
-import styles from '../styles/styles.module.css';
+import styles from "../styles/styles.module.css";
 
 export const ProductContext = createContext({} as ProductContextProps);
 const { Provider } = ProductContext;
 
 export const ProductCard = ({ children, product }: ProductCardProps) => {
-  const {counter, increaseBy} = useProduct();
+	const { counter, increaseBy } = useProduct();
 
-  return (
-    <div className={ styles.productCard }>
-      <Provider value={{
-        counter,
-        increaseBy,
-        product
-      }}>
-        { children }
-      </Provider>
-
-    </div>
-  )
-}
-
-ProductCard.Title = ProductTitle;
-ProductCard.Image = ProductImage;
-ProductCard.Buttons = ProductButtons;
+	return (
+		<div className={styles.productCard}>
+			<Provider
+				value={{
+					counter,
+					increaseBy,
+					product,
+				}}
+			>
+				{children}
+			</Provider>
+		</div>
+	);
+};
