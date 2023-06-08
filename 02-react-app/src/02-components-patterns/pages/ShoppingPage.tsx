69334cc9ce1964c8ccb3d0f7f076beb862a9e3e0
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-06-08 04:32:03 -0500

    before

--- 02-react-app/src/02-components-patterns/pages/ShoppingPage.tsx
+++ 02-react-app/src/02-components-patterns/pages/ShoppingPage.tsx
@@ -16,32 +16,19 @@ export const ShoppingPage = () => {
       <h1>Shopping Store</h1>
       <hr />
         <ProductCard
-          className="bg-dark text-white"
           key={ product.id }
           product={ product }
           initialValues={{
             count: 4,
-            maxCount: 10
+            // maxCount: 10
           }}
         >
           {
             ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
               <>
-                <ProductImage
-                  className="custom-image"
-                  style={{
-                    boxShadow: "10px 10px 10px rgba(0,0,0,0.2)",
-                  }}
-                />
-                <ProductTitle className="text-white text-bold" />
-                <ProductButtons className="custom-buttons" />
-                
-                <button onClick={ reset }> Reset </button>
-                <button onClick={ () => increaseBy(-2) }> -2 </button>
-                {
-                  ( !isMaxCountReached && <button onClick={ () => increaseBy(+2) }> +2 </button> )
-                }
-                <span>{ count } - { maxCount }</span>
+                <ProductImage />
+                <ProductTitle />
+                <ProductButtons />
               </>
             )
           }
