d38b567765f048f8b894766d44dd5d02d3bdd6f1
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2023-06-24 14:53:27 -0500

    Final testing

--- lfgc851-product-card/example/index.tsx
+++ lfgc851-product-card/example/index.tsx
@@ -1,13 +1,34 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
+
+const product = {
+  id: "1",
+  title: "Coffee Mug - Card",
+};
+
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <>
+      <ProductCard
+        product={ product }
+        initialValues={{
+          count: 4,
+        }}
+      >
+        {
+          ({ reset, count, isMaxCountReached, maxCount, increaseBy }) => (
+            <>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
+            </>
+          )
+        }
+      </ProductCard>
+    </>
   );
 };
 
